b058f06fc184496a5f9cae541034cbce7a325b0a
Sergio Segrera <sergioarturosegrera@gmail.com>
2024-01-27 15:23:55 -0500

    Connect pyqt to opencv

--- ui.py
+++ ui.py
@@ -6,6 +6,7 @@ from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
 import numpy as np
 import cv2
+import mediapipe as mp
 
 BACKGROUND_COLOR = "#71B48D"
 PRIMARY_COLOR = "#86CB92"
@@ -14,13 +15,72 @@ SECONDARY_COLOR = "#F2F2F2"
 class VideoThread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray)
 
+    def __init__(self):
+        super().__init__()
+        self.cap = cv2.VideoCapture(0)
+        #self.detector = mp.pose.Pose()
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.results = None
+        self.img = None
+        self.imgRGB = None
+        self.lmList = None
+        self.pose = None
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(False, 1, True,
+                                     False, True,
+                                     0.5, 0.5)
+
     def run(self):
         # capture from web cam
-        cap = cv2.VideoCapture(0)
-        while True:
-            ret, cv_img = cap.read()
+        while self.cap.isOpened():
+            ret, self.img = self.cap.read()
+            if not ret:
+                print("Failed to grab frame")
+                break
+
+            self.imgRGB = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+
+            self.img = self.find_Pose(self.img)
+
             if ret:
-                self.change_pixmap_signal.emit(cv_img)
+                self.change_pixmap_signal.emit(self.img)
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+    
+    def find_Pose(self, img, draw=True):
+        self.results = self.pose.process(self.imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img,self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+                
+        return img
+    
+    def calculate_angle(self, a, b, c):
+        a = np.array(a)
+        b = np.array(b)
+        c = np.array(c)
+        
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+        
+        if angle > 180.0:
+            angle = 360-angle
+            
+        return angle
+
+    def handle_lunge(self):
+        self.l_s = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.LEFT_SHOULDER]
+        self.l_hi = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.LEFT_HIP]
+        self.l_k = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.LEFT_KNEE]
+        self.l_a = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.LEFT_ANKLE]
+        self.r_s = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_SHOULDER]
+        self.r_hi = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_HIP]
+        self.r_k = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_KNEE]
+        self.r_a = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_ANKLE]
 
 
 class App(QWidget):
@@ -49,7 +109,6 @@ class App(QWidget):
         """)
 
         self.button = QPushButton('Start', self)
-
 
         # create a vertical box layout and add the two labels
         vbox = QVBoxLayout()
