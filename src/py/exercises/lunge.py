c9550833c8680284071e192618975d413da7b0be
Roko <roko,baljak@mail.mcgill.ca>
2024-01-27 16:15:19 -0500

    Finished basic lunge

--- src/py/exercises/lunge.py
+++ src/py/exercises/lunge.py
@@ -85,6 +85,11 @@ class camera: # class for the camera, so that we can use it to display the camer
         return magnitude_ab
 
     def handle_lunge(self):
+        # Constants
+        min_i_angle, max_i_angle = 75, 90
+        min_r_angle, max_r_angle = 80, 100
+        min_s_angle = 160
+        max_b_distance = 0.1
         # Landmarks
         self.l_s = self.get_xyz(self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.LEFT_SHOULDER])
         self.l_hi = self.get_xyz(self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.LEFT_HIP])
@@ -110,14 +115,31 @@ class camera: # class for the camera, so that we can use it to display the camer
         # print(f"L Knee angle: {self.la1}   |   R Knee angle: {self.ra1}")
 
         # Check if back is straight
-        if self.l_back < 0.1 and self.r_back < 0.1:
+        if self.l_back < max_b_distance and self.r_back < max_b_distance:
             print("Back is straight")
         else:
             print("Back is not straight")
-            
+
+        # Check start position
+        if self.la0 >= min_s_angle and self.ra0 >= min_s_angle:
+            print("Start position is correct")
+        else:
+            print("Start position is incorrect")
         # Check left lunge
+        if self.la0 <= max_r_angle and self.la0 >= min_r_angle and self.la1 >= min_i_angle and self.la1 <= max_i_angle:
+            if self.la0 >= min_s_angle and self.la1 >= min_r_angle and self.la1 <= max_r_angle:
+                print("Left lunge is correct")
+        else:
+            print("Left lunge is incorrect")
 
         # Check right lunge
+        if self.ra0 <= max_r_angle and self.ra0 >= min_r_angle and self.ra1 >= min_i_angle and self.ra1 <= max_i_angle:
+            if self.ra0 >= min_s_angle and self.ra1 >= min_r_angle and self.ra1 <= max_r_angle:
+                print("Right lunge is correct")
+        else:
+            print("Right lunge is incorrect")
+
+        
 
 
         
