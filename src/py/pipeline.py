d4cf552ed799a3907b0dbb5b71fd4c3a20ddc827
lshiyan <109442784+lshiyan@users.noreply.github.com>
2024-01-27 14:43:56 -0500

    pipeline

---
+++ src/py/pipeline.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+from pathlib import Path
+import os
+
+client = OpenAI(api_key="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")
+
+def whisperTranscript(filename): 
+  audio_file= open("../../audio/input/"+filename, "rb")
+  transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file,
+  )
+  return transcript
+
+def gptResponse(transcript_text): 
+    response = client.chat.completions.create(
+    model="gpt-4-0125-preview",
+    messages=[
+        {"role": "user", "content": transcript_text+"Do it in a short paragraph."},
+    ]
+    )
+    response_message=(response.choices[0].message.content)
+    return response_message
+  
+def tts(gptResponse):
+  speech_file_path = "../../audio/output/prompt-output.mp3"
+  response = client.audio.speech.create(
+    model="tts-1",
+    voice="shimmer",
+    input=gptResponse
+  )
+  
+  response.write_to_file(speech_file_path)
+  
+def getResponseFromInput(filename): #This costs me money every time it runs, do not run it too much. :(
+    transcriptedText=whisperTranscript(filename)
+    response=gptResponse(transcriptedText.text)
+    tts(response)
