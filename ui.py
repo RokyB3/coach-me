25f29f2d74a48c828ad126b3f8ab2c0bed9d5052
lshiyan <109442784+lshiyan@users.noreply.github.com>
2024-01-28 01:22:38 -0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -15,6 +15,7 @@ import mediapipe as mp
 import sounddevice as sd
 import soundfile as sf
 import pygame
+import math
 
 BACKGROUND_COLOR = "#71B48D"
 PRIMARY_COLOR = "#86CB92"
@@ -89,7 +90,84 @@ class VideoThread(QThread):
         self.r_hi = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_HIP]
         self.r_k = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_KNEE]
         self.r_a = self.results.pose_landmarks.landmark[self.mpPose.PoseLandmark.RIGHT_ANKLE]
+
+class camera: # class for the camera, so that we can use it to display the camera and get all the joints from it
+    def __init__(self):
+        self.start = True
+        self.counter = 0
+        self.cap = cv2.VideoCapture(0)
+        #self.detector = mp.pose.Pose()
+        self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.results = None
+        self.img = None
+        self.imgRGB = None
+        self.lmList = None
+        self.pose = None
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(False, 1, True,
+                                     False, True,
+                                     0.7, 0.7)
+
+    def display_camera(self): # displays the camera
+        while self.cap.isOpened():
+            ret, self.img = self.cap.read()
+            if not ret:
+                print("Failed to grab frame")
+                break
+            self.imgRGB = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            # self.results = self.detector.process(self.img)
+            self.results = self.pose.process(self.img)
+            self.lms = self.get_LM_positions(self.img)
+            if self.results.pose_landmarks:
+                self.handle_pullup()
+
+            cv2.imshow("Image", self.img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        self.cap.release()
+        cv2.destroyAllWindows()
+        return
+
+
+    def get_LM_positions(self, img):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                #finding height, width of the image printed
+                h, w, _ = img.shape
+                #Determining the pixels of the landmarks
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
         
+        self.mpDraw.draw_landmarks(img,self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return lmList
+
+
+    def calculate_2d_angle(self, p1, p2, p3):
+
+        # Get ladmark positions
+        x1, y1 = self.lms[p1][1:]
+        x2, y2 = self.lms[p2][1:]
+        x3, y3 = self.lms[p3][1:]
+        
+        # Calculate the angle
+        angle = math.degrees(math.atan2(y3-y2, x3-x2) - 
+                             math.atan2(y1-y2, x1-x2))
+        
+        # Convert to positive angle (Mirror to 180 degrees)
+        if angle < 0:
+            angle += 360
+            if angle > 180:
+                angle = 360 - angle
+        elif angle > 180:
+            angle = 360 - angle
+        
+        return angle 
+    
+    
 class MicrophoneWidget(QWidget):
     def __init__(self, container, parent=None):
         super().__init__(parent)
